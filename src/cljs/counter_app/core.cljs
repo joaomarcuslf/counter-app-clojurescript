5293f020533ffbb5b7efbfcac2faa61b14485d60
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2017-06-25 14:15:18 -0300

    Added local state components

--- src/cljs/counter_app/core.cljs
+++ src/cljs/counter_app/core.cljs
@@ -7,25 +7,30 @@
 (def app-state
   (reagent/atom 0))
 
-
-
 ;; Components
 
-(defn counting-component [on-change text]
-   [:a.column { :on-click #(swap! app-state on-change) }
-    text])
-
-(defn counter []
-  [:div.columns.is-mobile.has-text-centered
-    [counting-component dec "-"]
-    [:span.column.title.is-2 " " @app-state " "]
-    [counting-component inc "+"]
+(defn counting-component [text on-change state]
+  [:a.column { :on-click #(swap! state on-change) }
+    text
   ]
 )
 
+(defn counter []
+  (let [counter-state (reagent/atom 0)]
+    (fn []
+      [:div.columns.is-mobile.has-text-centered
+        [counting-component "-" dec counter-state]
+        [:span.column.title.is-2 " " @counter-state " "]
+        [counting-component "+" inc counter-state]
+      ]
+    )
+  )
+)
+
+
 ;; Main Page
 
-(defn page [ratom]
+(defn page []
   [:div.hero.is-primary.is-fullheight
     [:div.hero-head
       [:header.nav
@@ -40,22 +45,24 @@
     [:div.hero-body
       [:div.container.is-fullwidth.has-text-centered
         [counter]
-        [counting-component (fn [val] (- val val)) "Reset"]
+        [counter]
       ]
     ]
-  ])
+  ]
+)
 
 ;; Initialize App
 
 (defn dev-setup []
   (when ^boolean js/goog.DEBUG
     (enable-console-print!)
-    (println "dev mode")))
+    (println "dev mode")
+  )
+)
 
 
 (defn reload []
-  (reagent/render [page app-state]
-                  (.getElementById js/document "app")))
+  (reagent/render [page] (.getElementById js/document "app")))
 
 (defn ^:export main []
   (dev-setup)
