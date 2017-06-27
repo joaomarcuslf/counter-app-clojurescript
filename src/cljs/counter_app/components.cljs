f0df244c26f2425871dbab0ba180a8d66c89df06
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2017-06-26 23:44:53 -0300

    Changed component place

--- src/cljs/counter_app/components.cljs
+++ src/cljs/counter_app/components.cljs
@@ -3,3 +3,9 @@
 (defn counting-component [text on-change state]
  [:a.column { :on-click #(swap! state on-change)}
   text])
+
+(defn counter [counter-state]
+ [:div.columns.is-mobile.has-text-centered
+  [counting-component "-" dec counter-state]
+  [:span.column.title.is-2 " " @counter-state " "]
+  [counting-component "+" inc counter-state]])
--- src/cljs/counter_app/containers.cljs
+++ src/cljs/counter_app/containers.cljs
@@ -5,9 +5,4 @@
 
 
 (defn counter []
- (let [counter-state (reagent/atom 0)]
-  (fn []
-   [:div.columns.is-mobile.has-text-centered
-    [components/counting-component "-" dec counter-state]
-    [:span.column.title.is-2 " " @counter-state " "]
-    [components/counting-component "+" inc counter-state]])))
+ (let [counter-state (reagent/atom 0)] [components/counter counter-state]))
